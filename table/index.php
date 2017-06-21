989bcffee11b230fe7ddfde81eb332f25d5e5737
bartekwojtal1207 <bartekwojtal1207@gmail.com>
2017-06-21 17:07:46 +0200

    update to huzar project
    add table with 150 row... wait for data to table .... wait

---
+++ table/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>huzar - table</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <?php
+    // Silence is golden.
+      echo "hello world";
+    ?>
+  </body>
+</html>
